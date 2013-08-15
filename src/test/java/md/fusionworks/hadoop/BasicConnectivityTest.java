b66a9cc72e1b2447ab3a73497e08cd9cdff2f27d
gganebnyi <gganebnyi@fusionworks.md>
2013-08-15 14:40:11 +0300

    some fixes

--- src/test/java/md/fusionworks/hadoop/BasicConnectivityTest.java
+++ src/test/java/md/fusionworks/hadoop/BasicConnectivityTest.java
@@ -2,20 +2,25 @@ package md.fusionworks.hadoop;
 
 import java.io.IOException;
 
+import junit.framework.Assert;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.applications.distributedshell.ApplicationMaster;
+import org.apache.hadoop.yarn.applications.distributedshell.Client;
 import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
 import org.junit.Test;
 
 public class BasicConnectivityTest {
-
 	
+	protected static Configuration conf = new YarnConfiguration();
+
 	@Test
 	public void test() throws YarnException, IOException {
 		YarnClient client = YarnClient.createYarnClient();  
-		client.init(new Configuration());
+		client.init(conf);
 		client.start();
 		client.createApplication();
-		
 	}
 }
